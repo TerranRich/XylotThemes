a029dc6c39d33aebb8fe22059713ff5804a2b19f
Richard Brum <rbrum@addventures.com>
2019-05-16 13:52:43 -0400

    Better logging

--- web/scripts/hooks/git-pull.php
+++ web/scripts/hooks/git-pull.php
@@ -2,10 +2,10 @@
 
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
-if ($_POST['payload']) {
+if (@$_POST['payload']) {
   shell_exec( `cd /var/www/xylot/v7/ && git reset --hard HEAD && git pull && drush cr` );
 }
 else {
-  error_log('Tried running scripts/hooks/git-pull.php, no payload apparent.');
+  error_log('Tried running scripts/hooks/git-pull.php, no payload apparent. $_POST indexes available: ' . implode(', ', array_keys($_POST)));
   die('No payload was given. This sucks.');
 }
