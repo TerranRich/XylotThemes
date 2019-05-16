5907047463e46f1fc2b60fc253f3f8c9ea610a55
Richard Brum <rbrum@addventures.com>
2019-05-16 13:51:27 -0400

    Adding log to see what is maybe going on.

--- web/scripts/hooks/git-pull.php
+++ web/scripts/hooks/git-pull.php
@@ -6,5 +6,6 @@ if ($_POST['payload']) {
   shell_exec( `cd /var/www/xylot/v7/ && git reset --hard HEAD && git pull && drush cr` );
 }
 else {
+  error_log('Tried running scripts/hooks/git-pull.php, no payload apparent.');
   die('No payload was given. This sucks.');
 }
