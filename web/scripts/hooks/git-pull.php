0b657a179b04248407487cf5022dd84f208621c7
Richard Brum <rbrum@addventures.com>
2019-05-16 13:53:20 -0400

    Testing push event again

--- web/scripts/hooks/git-pull.php
+++ web/scripts/hooks/git-pull.php
@@ -1,7 +1,6 @@
 <?php
 
 // Use in the "Post-Receive URLs" section of your GitHub repo.
-
 if (@$_POST['payload']) {
   shell_exec( `cd /var/www/xylot/v7/ && git reset --hard HEAD && git pull && drush cr` );
 }
