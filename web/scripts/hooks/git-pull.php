111a2573d638c2134658e75b0bfac5cd48e907e5
Richard Brum <rbrum@addventures.com>
2019-05-16 13:56:26 -0400

    Hopefully this is the final edit of the hook.

--- web/scripts/hooks/git-pull.php
+++ web/scripts/hooks/git-pull.php
@@ -1,8 +1,13 @@
 <?php
 
 // Use in the "Post-Receive URLs" section of your GitHub repo.
-if (@$_POST['payload']) {
-  shell_exec( `cd /var/www/xylot/v7/ && git reset --hard HEAD && git pull && drush cr` );
+if (@$_POST['repository']) {
+  if (@$_POST['repository']['name'] == 'XylotThemes') {
+    shell_exec( `cd /var/www/xylot/v7/ && git reset --hard HEAD && git pull && drush cr` );
+  }
+  else {
+    error_log('Invalid information given in GitHub payload (' . __FILE__ . ')');
+  }
 }
 else {
   error_log('Tried running scripts/hooks/git-pull.php, no payload apparent. $_POST indexes available: ' . implode(', ', array_keys($_POST)));
