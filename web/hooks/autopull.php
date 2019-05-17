f0ae1e08e47074e7ef15b559b231e31b8880acf5
Richard Brum <rbrum@addventures.com>
2019-05-17 13:08:17 -0400

    Hopefully this is the final webhook script

--- web/hooks/autopull.php
+++ web/hooks/autopull.php
@@ -2,7 +2,12 @@
 
 echo '-- Starting auto-pull on server --', "\n";
 
+// Change directory to project root
 `cd /var/www/xylot/v7`;
-`git pull`;
+
+// Pull latest master
+`git checkout master`;
+`git reset --hard HEAD`;
+`git pull -f`;
 
 echo '-- Ending auto-pull. --', "\n";
